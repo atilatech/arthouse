7a858d4f42e37691ad320eb116f5cf3bbe22df77
ademidun <tomiademidun@gmail.com>
2022-05-24 10:55:48 -0400

    use Moralis NFT API to create NFTs for a given address

--- src/components/NFTCard.tsx
+++ src/components/NFTCard.tsx
@@ -2,6 +2,7 @@ import { Button, Tag } from 'antd';
 import React from 'react'
 import { CONFIG_CHAINS } from '../config';
 import { NFTMetadata } from '../models/NFT';
+import "./NFTCard.scss";
 
 function NFTCard({nft, chainId}: {nft: NFTMetadata, chainId: string}) {
 
@@ -10,14 +11,14 @@ const activeChain = CONFIG_CHAINS[chainId];
 const nftBlockExplorerUrl = `${activeChain.BLOCK_EXPLORER_URL}/${activeChain.CHAIN_NAME !== "Harmony" ? "token": "address"}/${activeChain.NFT_ADDRESS}?a=${nft.tokenId}`;
 
   return (
-    <div className="card shadow">
+    <div className="NFTCard card shadow">
     <img src={nft.image}  alt={nft.name} width="auto" height="300" className="m-3"/>
     <div className="p-4">
     <h3 className="text-2xl font-semibold">{nft.name}</h3>
     <hr/>
 
     <div>
-        <p className="text-gray-400">{nft.description}</p>
+        <p className="description">{nft.description}</p>
     </div>
     <hr/>
 
--- src/scenes/NFT/MyNFTs.tsx
+++ src/scenes/NFT/MyNFTs.tsx
@@ -1,109 +1,84 @@
 /* pages/my-assets.js */
 import React, { ethers } from 'ethers'
 import { useEffect, useState } from 'react'
-import axios from 'axios'
 import Web3Modal from "web3modal"
-import {
-    NFT_ADDRESS, NFT_MARKETPLACE_ADDRESS
-} from '../../config';
+import Moralis from 'moralis';
+import { components } from 'moralis/types/generated/web3Api';
+import { Col, Row, Spin } from 'antd';
+import NFTCard from '../../components/NFTCard';
+import { NFTMetadata } from '../../models/NFT';
 
-import Market from '../../artifacts/contracts/Market.sol/NFTMarket.json'
-import NFT from '../../artifacts/contracts/NFT.sol/NFT.json'
-import CreatedNFTs from './CreatedNFTs';
 
 export default function MyNFTs() {
 
   const [nfts, setNfts] = useState<any>([]);
-  const [createdNfts, setCreatedNfts] = useState<any>([]);
-  const [soldNfts, setSoldNfts] = useState<any>([]);
   const [loadingState, setLoadingState] = useState('not-loaded');
-  const [loadingCreatedState, setLoadingCreatedState] = useState('not-loaded');
 
   useEffect(() => {
     loadNFTs()
-  }, [])
+  }, []);
+
   async function loadNFTs() {
-    const web3Modal = new Web3Modal({
-      network: "mainnet",
-      cacheProvider: true,
-    })
+
+    const web3Modal = new Web3Modal()
     const connection = await web3Modal.connect()
-    const provider = new ethers.providers.Web3Provider(connection)
-    const signer = provider.getSigner()
+    const provider = new ethers.providers.Web3Provider(connection)    
+    const signer = provider.getSigner();
+    const address = await signer.getAddress();
 
-    const marketContract = new ethers.Contract(NFT_MARKETPLACE_ADDRESS, Market.abi, signer)
-    const tokenContract = new ethers.Contract(NFT_ADDRESS, NFT.abi, provider)
-    const data = await marketContract.fetchMyNFTs();
+    setLoadingState(`Loading NFTs for Rinkeby`);
+    const options = {
+      // needed to resolve typescript compilation errors because "chain" was being interpreted as a string that can accept many values
+      // Types of property 'chain' are incompatible.
+      // Type 'string' is not assignable to type '"rinkeby" | "eth" | "0x1" | "ropsten" | "0x3" | ... N more ... |
+      // see: https://bobbyhadz.com/blog/typescript-type-string-is-not-assignable-to-type
+      chain: "rinkeby" as components["schemas"]["chainList"],
+      address,
+    };
+    const data = await Moralis.Web3API.account.getNFTs(options);
 
-    const items = await Promise.all(data.map(async (i: any) => {
-      const tokenUri = await tokenContract.tokenURI(i.tokenId)
-      const meta = await axios.get(tokenUri)
-      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+    const items = data.result!.filter(nft => nft.metadata).map((token) => {
+      const metadata = JSON.parse(token.metadata || "{}");
+      const { name, description, image } = metadata;
+      // let price = token.price ? ethers.utils.formatUnits(token.price.toString(), 'ether') : "";
       let item = {
-        price,
-        tokenId: i.tokenId.toNumber(),
-        seller: i.seller,
-        owner: i.owner,
-        image: meta.data.image,
+        // price,
+        tokenId: Number.parseInt(token.token_id),
+        // seller: token.seller,
+        owner: token.owner_of,
+        name,
+        description,
+        image,
       }
       return item
-    }))
+    })
+
     setNfts(items)
     setLoadingState('loaded');
 
-    loadCreatedNFTs(marketContract, tokenContract);
-
   };
-
-  async function loadCreatedNFTs(marketContract: ethers.Contract, tokenContract: ethers.Contract) {
-
-
-    const data = await marketContract.fetchMyNFTs();
-    const items = await Promise.all(data.map(async (i: any) => {
-        const tokenUri = await tokenContract.tokenURI(i.tokenId)
-        const meta = await axios.get(tokenUri)
-        let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
-        let item = {
-          price,
-          tokenId: i.tokenId.toNumber(),
-          seller: i.seller,
-          owner: i.owner,
-          sold: i.sold,
-          image: meta.data.image,
-        }
-        return item
-      }));
-
-      /* create a filtered array of items that have been sold */
-      const soldItems = items.filter(i => i.sold)
-      setSoldNfts(soldItems)
-      setCreatedNfts(items)
-      setLoadingCreatedState('loaded') 
-  };
-
-  if (loadingState === 'loaded' && !nfts.length) return (<h1 className="py-10 px-20 text-3xl">No assets owned</h1>)
 
   return (
     <div className="MyNFTs card shadow container p-5">
       <h1>My NFTs</h1>
-
-      <div className="p-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-          {
-            nfts.map((nft: any, i: number) => (
-              <div key={i} className="border shadow rounded-xl overflow-hidden">
-                <img src={nft.image} className="rounded" />
-                <div className="p-4 bg-black">
-                  <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
-                </div>
-              </div>
-            ))
-          }
-        </div>
-      </div>
-        
-     {loadingCreatedState === 'loaded' && <CreatedNFTs createdNfts={createdNfts} soldNfts={soldNfts} />}
+      {loadingState === 'loaded' && !nfts.length &&
+       <h1 className="py-10 px-20 text-3xl">No assets owned</h1>
+      }
+      {loadingState?.toLowerCase().includes("loading") && <Spin size="large"  tip={loadingState}/>}
       
+      <Row gutter={[24,24]}>
+          {
+            nfts.map((nft: NFTMetadata, i: number) => {
+
+              return(
+                <Col md={8} sm={24} key={i}>
+                  <NFTCard nft={nft} chainId={"4"} />
+                </Col>
+              )
+
+            })
+          }
+      </Row>
     </div>
   )
 }
