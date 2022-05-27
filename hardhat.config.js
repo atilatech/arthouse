46aff50c70ca0f3c382a47eb497b3f95f1fb376a
ademidun <tomiademidun@gmail.com>
2022-05-27 19:19:42 -0400

    update environment variable name for CONTRACT_DEPLOYMENT_WALLET_PRIVATE_KEY to make it more explicit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 /* hardhat.config.js */
 require("@nomiclabs/hardhat-waffle")
-const privateKey = process.env.privateKey;
+const CONTRACT_DEPLOYMENT_WALLET_PRIVATE_KEY = process.env.CONTRACT_DEPLOYMENT_WALLET_PRIVATE_KEY;
 const rpcApiKeyMumbai = process.env.rpcApiKeyMumbai;
 
 module.exports = {
@@ -23,37 +23,37 @@ module.exports = {
     },
     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${rpcApiKeyMumbai}`,
-      accounts: [`0x${privateKey}`],
+      accounts: [`0x${CONTRACT_DEPLOYMENT_WALLET_PRIVATE_KEY}`],
       chainId: 80001
     },
     alfajores: {
       url: `https://alfajores-forno.celo-testnet.org`,
-      accounts: [`0x${privateKey}`],
+      accounts: [`0x${CONTRACT_DEPLOYMENT_WALLET_PRIVATE_KEY}`],
       chainId: 44787
     },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${rpcApiKeyMumbai}`,
-      accounts: [`0x${privateKey}`],
+      accounts: [`0x${CONTRACT_DEPLOYMENT_WALLET_PRIVATE_KEY}`],
       chainId: 4
     },
     harmonytestnet: {
       url: `https://api.s0.b.hmny.io`,
-      accounts: [`0x${privateKey}`],
+      accounts: [`0x${CONTRACT_DEPLOYMENT_WALLET_PRIVATE_KEY}`],
       chainId: 1666700000
     },
     bsctestnet: {
       url: `https://data-seed-prebsc-1-s1.binance.org:8545`,
-      accounts: [`0x${privateKey}`],
+      accounts: [`0x${CONTRACT_DEPLOYMENT_WALLET_PRIVATE_KEY}`],
       chainId: 97
     },
     binance: {
       url: `https://bsc-dataseed.binance.org`,
-      accounts: [`0x${privateKey}`],
+      accounts: [`0x${CONTRACT_DEPLOYMENT_WALLET_PRIVATE_KEY}`],
       chainId: 56
     },
     polygon: {
       url: `https://polygon-rpc.com`,
-      accounts: [`0x${privateKey}`],
+      accounts: [`0x${CONTRACT_DEPLOYMENT_WALLET_PRIVATE_KEY}`],
       chainId: 137
     }
   },
