dcbb8d6d7587b03518f1aa22ed0f8e4dd7b304e2
ademidun <tomiademidun@gmail.com>
2022-07-12 20:40:41 -0400

    add settings page

---
+++ src/scenes/Settings/Settings.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Settings() {
+  return (
+    
+    <div className="container">
+        <h1 className='text-center'>
+            Settings
+        </h1>
+        
+    </div>
+  )
+}
+
+export default Settings
